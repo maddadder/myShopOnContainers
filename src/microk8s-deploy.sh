5a2b6f76068d6e73e8145926fbc76bb7be7287ff
maddadder <rleecharlie@gmail.com>
2023-09-20 18:07:29 -0700

    simplify deployment script

--- src/microk8s-deploy.sh
+++ src/microk8s-deploy.sh
@@ -3,25 +3,25 @@ set -e
 
 source .microk8s
 #if false; then
-docker-compose --env-file .microk8s -f docker-compose-microk8s.yml build
+docker-compose --env-file .microk8s build
 
-docker push $REGISTRY/identity.api:$TAG
-docker push $REGISTRY/basket.api:$TAG
-docker push $REGISTRY/catalog.api:$TAG
-docker push $REGISTRY/ordering.api:$TAG
-docker push $REGISTRY/ordering.backgroundtasks:$TAG
-docker push $REGISTRY/payment.api:$TAG
-docker push $REGISTRY/webhooks.api:$TAG
-docker push $REGISTRY/mobileshoppingagg:$TAG
-docker push $REGISTRY/webshoppingagg:$TAG
-docker push $REGISTRY/ordering.signalrhub:$TAG
-docker push $REGISTRY/webstatus:$TAG
-docker push $REGISTRY/webspa:$TAG
-docker push $REGISTRY/webmvc:$TAG
-docker push $REGISTRY/webhooks.client:$TAG
+docker push $REGISTRY/identity.api:linux-$TAG
+docker push $REGISTRY/basket.api:linux-$TAG
+docker push $REGISTRY/catalog.api:linux-$TAG
+docker push $REGISTRY/ordering.api:linux-$TAG
+docker push $REGISTRY/ordering.backgroundtasks:linux-$TAG
+docker push $REGISTRY/payment.api:linux-$TAG
+docker push $REGISTRY/webhooks.api:linux-$TAG
+docker push $REGISTRY/mobileshoppingagg:linux-$TAG
+docker push $REGISTRY/webshoppingagg:linux-$TAG
+docker push $REGISTRY/ordering.signalrhub:linux-$TAG
+docker push $REGISTRY/webstatus:linux-$TAG
+docker push $REGISTRY/webspa:linux-$TAG
+docker push $REGISTRY/webmvc:linux-$TAG
+docker push $REGISTRY/webhooks.client:linux-$TAG
 #fi
 
-#helm install eshop ../deploy/microk8s/chart --set registry.url=$REGISTRY --namespace $NAMESPACE
-helm upgrade eshop ../deploy/microk8s/chart --set registry.url=$REGISTRY --namespace $NAMESPACE
+helm install eshop ../deploy/microk8s/chart --set registry.url=$REGISTRY --set image.tag=linux-$TAG --namespace $NAMESPACE
+#helm upgrade eshop ../deploy/microk8s/chart --set registry.url=$REGISTRY --set image.tag=linux-$TAG --namespace $NAMESPACE
 
 #helm uninstall eshop --namespace $NAMESPACE
